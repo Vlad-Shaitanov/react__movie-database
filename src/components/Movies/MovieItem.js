6f38770e76cc9bb1ee9b288df388c3d74571dd88
Vladislav <kelsenelenelvial@mail.ru>
2021-09-01 23:08:13 +0300

    Icons in a MovieItems

--- src/components/Movies/MovieItem.js
+++ src/components/Movies/MovieItem.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
 
 export class MovieItem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      icons: {
+        favorite: false, //По умолчанию иконки избранного вишлиста неактивны
+        bookmark: false,
+      },
+    };
+  }
+
+  toggleFavIcon = () => {
+    this.setState((prevState) => ({
+      icons: {
+        ...this.state.icons,
+        favorite: !this.state.icons.favorite,
+      },
+    }));
+  };
+
+  toggleBookmarkIcon = () => {
+    this.setState((prevState) => ({
+      icons: {
+        ...this.state.icons,
+        bookmark: !this.state.icons.bookmark,
+      },
+    }));
+  };
+
+  // todo Подумать над местом хранения состояния
+
   render() {
     const { item } = this.props;
+    const {
+      icons: { favorite, bookmark },
+    } = this.state;
     const imagePath = item.backdrop_path || item.poster_path;
     // console.log(item);
     return (
@@ -13,6 +47,24 @@ export class MovieItem extends Component {
           alt=""
         />
         <div className="card-body">
+          <div className="card-icons">
+            <span
+              className="material-icons"
+              title="В Избранное"
+              onClick={this.toggleFavIcon}
+            >
+              {/*star_border*/}
+              {favorite ? "star" : "star_border"}
+            </span>
+            <span
+              className="material-icons"
+              title="Смотреть позже"
+              onClick={this.toggleBookmarkIcon}
+            >
+              {/*bookmark_border*/}
+              {bookmark ? "bookmark" : "bookmark_border"}
+            </span>
+          </div>
           <h6 className="card-title">{item.title}</h6>
           <div className="card-text">Рейтинг: {item.vote_average}</div>
         </div>
