46aec6c63e1bcaa1fc560fbe2919cb153bd8207e
Vladislav <kelsenelenelvial@mail.ru>
2021-09-10 15:52:27 +0300

    Movie info

--- src/components/pages/MoviePage/MoviePage.js
+++ src/components/pages/MoviePage/MoviePage.js
@@ -1,4 +1,18 @@
 import React, { Component } from "react";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col,
+} from "reactstrap";
+import classnames from "classnames";
 import CallApi from "../../../api/api";
 
 export default class MoviePage extends Component {
@@ -7,12 +21,26 @@ export default class MoviePage extends Component {
 
     this.state = {
       movie: {},
+      favList: false,
+      watchList: false,
     };
   }
 
   updateMovie = (movie) => {
     this.setState({
       movie,
+    });
+  };
+
+  toggleFav = () => {
+    this.setState({
+      favList: !this.state.favList,
+    });
+  };
+
+  toggleWatch = () => {
+    this.setState({
+      watchList: !this.state.watchList,
     });
   };
 
@@ -48,18 +76,31 @@ export default class MoviePage extends Component {
         runtime,
         overview,
         genres,
+        budget,
+        revenue,
       },
+      favList,
+      watchList,
     } = this.state;
 
     //backdrop_path - задний фон фильма
     //poster_path - обложка
-    console.log("movie", movie);
+    // console.log("movie", movie);
 
     const movieBackground = `https://image.tmdb.org/t/p/w500${backdrop_path}`;
-    // let arr = [];
-    // arr.push(genres);
-    // console.log(arr);
-    // arr.map((item) => console.log("item", item));
+    let genresList = [];
+
+    try {
+      genresList = genres
+        .map((item) => {
+          return item.name;
+        })
+        .join(", ");
+      // console.log(genresList);
+    } catch (error) {
+      console.log("Error", `${error.name} ${error.message}`);
+    }
+
     return (
       <div className="container pt-5">
         <div
@@ -69,26 +110,64 @@ export default class MoviePage extends Component {
           }}
         >
           <div className="col">
-            <div className="row bg-black bg-opacity-25">
+            <div className="row bg-black bg-opacity-50">
               <div className="col-4 p-3">
                 <img
                   src={`https://image.tmdb.org/t/p/w500${poster_path}`}
                   style={{ maxWidth: "100%" }}
                   className="border rounded-3"
+                  alt=""
                 />
               </div>
-              <div className="col-6 text-light">
-                <p className="h4">{title}</p>
+              <div className="col-8 text-light">
+                <p className="h4 text-center pt-3">{title}</p>
                 <p>Премьера: {release_date} </p>
-                <span>Продолжительность: {runtime} минут(ы) </span>
+                <p>{genresList}</p>
+                <p>Продолжительность: {runtime} минут(ы) </p>
+                <div className="d-flex justify-content-between">
+                  <p>Кассовые сборы: ${revenue}</p>
+                  <p>Бюджет: ${budget}</p>
+                </div>
+                <div className="d-flex icons w-25 justify-content-between">
+                  <div className="icons__item-bg">
+                    <span
+                      className="material-icons md-36 yellow"
+                      title="В Избранное"
+                      onClick={this.toggleFav}
+                    >
+                      {favList ? "favorite" : "favorite_border"}
+                    </span>
+                  </div>
+                  <div className="icons__item-bg">
+                    <span
+                      className="material-icons md-36"
+                      title="Смотреть позже"
+                      onClick={this.toggleWatch}
+                    >
+                      {watchList ? "bookmark" : "bookmark_border"}
+                    </span>
+                  </div>
+                </div>
                 <div>
-                  <h4>Обзор</h4>
+                  <h4 className="mt-3 text-center">Обзор</h4>
                   <p>{overview}</p>
-                  <p>{}</p>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className="row">
+          <Nav tabs>
+            <NavItem>
+              <NavLink className="">Подробнее</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="">Видео</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="">Актеры</NavLink>
+            </NavItem>
+          </Nav>
         </div>
       </div>
     );
